5549b4e691695ceb9bdd99dbdf041a98a18c7ee3
Julien Sicot <julien.sicot@gmail.com>
2023-01-31 08:57:26 +0100

    Update new-books.js

--- new-books.js
+++ new-books.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
     var json_file = "new-books.json";
   }
 
-  $.getJSON("data/"+json_file, function(data) {
+  $.getJSON("data/new-books.json", function(data) {
     var slides = [];
     $.each(data, function(i, book) {
       if (book["thumbnail"] != "" && book["thumbnail"] != "Too Many Requests") {
